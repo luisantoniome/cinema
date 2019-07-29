46073104577847a49becdfcaa4f684d22f8a0273
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2019-07-29 17:16:51 -0500

    First steps, main layout created

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hall/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hall from './components/Hall.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Hall
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Hall.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div class="screen">
+      <p class="font-weight-bold">Screen</p>
+    </div>
+
+    <div class="seats">
+      <div class="row">
+        <div class="col seat"
+          v-for="seat in seats"
+          :key="seat.id"
+          :id="seat.id"
+          v-text="seat.id"
+          :class="seat.available ? 'available' : 'unavailable'"
+          @click="selectSeat"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      seats: [
+        { id: 'A1', available: true },
+        { id: 'A2', available: true },
+        { id: 'A3', available: false },
+        { id: 'A4', available: false },
+        { id: 'A5', available: true },
+        { id: 'A6', available: true },
+      ]
+    }
+  },
+
+  methods: {
+    selectSeat (event) {
+      let seat = this.seats.find(seat => seat.id === event.target.id)
+      seat.available = !seat.available
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  .screen {
+    background-color: #2b6282;
+  }
+
+  .screen p {
+    color: white;
+    font-size: 18px;
+  }
+
+  .seats {
+    margin-top: 60px;
+  }
+
+  .seat {
+    color: white;
+    margin: 3px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .available {
+    background-color: #2d4d86;
+  }
+
+  .unavailable {
+    background-color: #73264f;
+  }
+</style>
